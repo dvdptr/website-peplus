2a8fdc89dac1a152c549a320ccfb316bcb25223e
dvdptr <info@davidebner.de>
2020-06-09 14:33:48 +0200

    add hero content

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="border-b border-gray-400 flex py-12 justify-around items-center">
-    <div class="w-5/12">
+  <div class="border-b border-gray-400 flex py-16 justify-around items-center">
+    <div class="w-1/2 mx-4">
       <section>
         <p class="text-5xl font-bold leading-tight pb-4">
           Finanzierung
           <br />leicht gemacht.
         </p>
         <p
-          class="text-gray-800 pb-4"
-        >Individuelle Beratung und optimale Lösungsvorschläge für Ihre Finanzierung.</p>
-        <p class="text-gray-800 pb-6">Ich bin gerne für Sie da.</p>
+          class="text-gray-800 text-lg pb-4 leading-snug"
+        >Sie benötigen individuelle Beratung für Ihre geplante Finanzierung? Auf dieser Webseite erhalten Sie Informationen rund um die Themen Finanzierung, Finanzieren ohne Eigenkapital und Privatkredite.</p>
+        <p class="text-gray-800 text-lg pb-6">Ich bin gerne für Sie da.</p>
         <button
           class="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
         >Anfrage stellen</button>
       </section>
     </div>
-    <div class="w-5/12">
+    <div class="w-1/2 px-4">
       <section>
         <div class="bg-blue-200 w-full h-64"></div>
       </section>
