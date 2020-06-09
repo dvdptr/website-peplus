b644fcfdef354666e8a59e22bf9ec33bef1d5bf1
dvdptr <info@davidebner.de>
2020-06-09 12:16:54 +0200

    add content to footer component; needs more styling and content

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,27 @@
 <template>
-  <div>
-    <section class="border-b border-gray-400 py-4">
-      <Logo class="h-8" />
-    </section>
+  <div
+    class="border-b border-gray-400 py-4 flex mx-auto justify-center text-gray-800 leading-tight text-sm"
+  >
+    <div class>
+      <section class="py-4 mr-12">
+        <Logo class="h-8" />
+      </section>
+    </div>
+    <div class="flex">
+      <section class="py-4 mr-12">
+        <p class="underline pb-2">Home-Office</p>Mobil: 000
+        <br />e-Mail: email
+        <br />
+      </section>
+      <section class="py-4 mr-12">
+        <p class="underline pb-2">Kontakt</p>Mobil: 000
+        <br />e-Mail: email
+      </section>
+      <section class="py-4">
+        <p class="underline pb-2">Links</p>Impressum
+        <br />Datenschutz
+      </section>
+    </div>
   </div>
 </template>
 
