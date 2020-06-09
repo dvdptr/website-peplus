8c59ab19e1c1183a65fc617ab90a2f061e28cc27
dvdptr <info@davidebner.de>
2020-06-09 11:13:23 +0200

    add content for Leistungen component; TODO: adjust box-size per unit and needs styling

--- components/Leistungen.vue
+++ components/Leistungen.vue
@@ -5,6 +5,28 @@
         <Headline :title="title" :subtitle="subtitle" />
       </div>
     </section>
+
+    <!-- Leistungen Loop -->
+    <div class="flex flex-wrap justify-around">
+      <div
+        class="border border-gray-400 rounded-sm w-5/12 mx-auto p-2 mb-3"
+        v-for="leistung in leistungenOverview"
+        v-bind:key="leistung"
+      >
+        <div class>
+          <div class="pb-2">
+            <img :src="leistung.iconUrl" alt class />
+            {{ leistung.headline }}
+          </div>
+          <div class="text-sm leading-snug text-gray-700 pb-2">{{ leistung.text }}</div>
+
+          <a
+            href="mailto:peterebner@gmx.de"
+            class="text-red-800 hover:font-bold text-sm"
+          >Termin vereinbaren &#8594;</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,7 +40,45 @@ export default {
   data() {
     return {
       title: "Leistungen im Überblick",
-      subtitle: "Überblick der Leistungen"
+      subtitle: "Überblick der Leistungen",
+      leistungenOverview: [
+        {
+          //   iconUrl: require("@/assets/svg/chevron-r.svg"),
+          headline: "Baufinanzierung",
+          text:
+            "Sie möchten eine Wohnung oder ein Haus kaufen und benötigen hierzu eine Finanzierung? Oder haben Sie konkrete Fragen zu Konditionen oder Höhe der monatlichen Rate? Gerne berate ich Sie rund um das Thema Baufinanzierung."
+        },
+        {
+          //   iconUrl: require("@/assets/svg/chevron-r.svg"),
+          headline: "Finanzieren ohne Eigenkapital",
+          text:
+            "Baufinanzierungen ohne eigene Mittel (Eigenkapital) werden immer häufiger nachgefragt. Dabei ermöglicht eine Kombination aus Eigenkapital-Ersatzdarlehen und Baufinanzierung den Start in die eigenen vier Wände. Wir können das ermöglichen."
+        },
+        {
+          //   iconUrl: require("@/assets/svg/chevron-r.svg"),
+          headline: "Privatkredit",
+          text:
+            "Sie benötigen einen Privatkredit zur freien Verwendung? Oder möchten Sie für einen bestehenden Kredit die Zinsen verbessern? Erkundigen Sie sich nach Sondertilgungen und zinsgünstigen Darlehen verschiedener Banken. Gerne helfe ich Ihnen weiter."
+        },
+        {
+          //   iconUrl: require("@/assets/svg/chevron-r.svg"),
+          headline: "Anschlussfinanzierung",
+          text:
+            "Ihre Zinsbindung läuft bald aus und eine Restschuld ist noch vorhanden? Profitieren Sie von einer maßgeschneiderten Lösung für Ihre Weiterfinanzierung oder sichern Sie sich schon heute Ihren Zinssatz für später (Forward-Darlehen)."
+        },
+        {
+          //   iconUrl: require("@/assets/svg/chevron-r.svg"),
+          headline: "Versicherungen",
+          text:
+            "Egal ob Berufsunfähigkeitsschutz, Sachversicherungen oder Rentenversicherung – Vorsorge ist wichtig! Erfahren Sie mehr rund um das Thema Absicherungen mit Hinblick auf Ihre persönliche Situation."
+        },
+        {
+          //   iconUrl: require("@/assets/svg/chevron-r.svg"),
+          headline: "Individuelle Beratung",
+          text:
+            "Gerne unterstütze ich Sie auch in schwierigen Fällen einer Finanzierung, die bspw. bei Ihrer Hausbank abgelehnt wurde. Nehmen Sie hierzu ganz einfach unverbindlich Kontakt auf und lassen Sie uns gemeinsam nach einer Lösung suchen."
+        }
+      ]
     };
   }
 };
