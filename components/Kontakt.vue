70fd0139e1911d1750f44149dee9bb86f4aa297c
dvdptr <info@davidebner.de>
2020-06-09 12:18:24 +0200

    adjust headline weight to font-bold

--- components/Kontakt.vue
+++ components/Kontakt.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="border-b border-gray-400 py-6">
+  <div class="border-b border-gray-400 py-10">
     <div class="flex justify-center mx-auto">
       <div class="w-5/12">
-        <section class="text-4xl pb-2">Sie haben Fragen?</section>
+        <section class="text-4xl pb-2 font-bold">Sie haben Fragen?</section>
         <section
           class="text-normal text-gray-700 pb-4"
         >Gerne berate ich Sie in einem persönlichen Gespräch. Nehmen Sie ganz unverbindlich Kontakt zu mir auf, ich freue mich von Ihnen zu hören!</section>
--- components/elements/Headline.vue
+++ components/elements/Headline.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-6">
-    <section class="text-center text-2xl">{{title}}</section>
+    <section class="text-center text-2xl font-bold">{{title}}</section>
     <section class="text-center text-gray-700 w-1/2 mx-auto leading-snug text-base">{{subtitle}}</section>
   </div>
 </template>
