2e6363e396af88ec36f2fa9ff4f108e734989ae1
dvdptr <info@davidebner.de>
2020-06-09 11:31:22 +0200

    add content to component Kontakt; needs more styling and content as it is just placeholder text for now

--- components/Kontakt.vue
+++ components/Kontakt.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <section class="border-b border-gray-400">Sie haben Fragen?</section>
+  <div class="border-b border-gray-400 py-6">
+    <div class="flex justify-center mx-auto">
+      <div class="w-5/12">
+        <section class="text-4xl pb-2">Sie haben Fragen?</section>
+        <section
+          class="text-normal text-gray-700 pb-4"
+        >Gerne berate ich Sie in einem persönlichen Gespräch. Nehmen Sie ganz unverbindlich Kontakt zu mir auf, ich freue mich von Ihnen zu hören!</section>
+        <button
+          class="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
+        >Anfrage stellen</button>
+      </div>
+      <section class="w-1/3 bg-red-100">*Image Placeholder</section>
+    </div>
   </div>
 </template>
 
