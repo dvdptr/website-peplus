50213292e4a2b2810ef34c5a92d2ca9d9c6c9ef3
dvdptr <info@davidebner.de>
2020-06-11 10:01:33 +0200

    add button to about component

--- components/About.vue
+++ components/About.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="border-b border-gray-400 pb-12">
     <Headline :title="title" :subtitle="subtitle" />
-    <div class="flex pt-12 items-center">
-      <div class="w-1/2">
+    <div class="flex pt-12 items-center justify-evenly">
+      <div class>
         <img src="@/assets/img/peterEbner.jpg" alt class="rounded-md mx-auto" />
       </div>
-      <div class="w-1/2">
+      <div class>
         <section class="pb-6">
           <p>Peter Ebner</p>
           <p>Bankkaufmann & Finanzierungsspezialist</p>
@@ -15,6 +15,11 @@
             <img :src="check.iconUrl" alt class="w-8 ml-2" />
             <p>{{check.item}}</p>
           </div>
+        </section>
+        <section class="pt-6">
+          <button
+            class="bg-white hover:bg-gray-100 text-gray-800 font-bold py-2 px-4 border border-gray-400 rounded shadow"
+          >Kontakt</button>
         </section>
       </div>
     </div>
