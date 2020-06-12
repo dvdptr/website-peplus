fa0b15f0bdbb2084f7a9f95199fdb9e3d09c92e5
dvdptr <info@davidebner.de>
2020-06-12 09:54:54 +0200

    add logo version able to adjust to text color by class for usage in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,37 +1,37 @@
 <template>
   <div
-    class="border-b border-gray-400 py-6 flex mx-auto justify-center text-gray-800 leading-tight text-sm bg-gray-800"
+    class="border-b border-gray-400 py-8 flex mx-auto justify-center text-white leading-tight text-sm bg-gray-600"
   >
     <div class>
       <section class="py-4 mr-12">
-        <Logo class="h-8" />
+        <LogoWhite class="h-10 fill-current text-white" />
       </section>
     </div>
     <div class="flex">
       <section class="py-4 mr-12">
-        <p class="underline pb-2">Home-Office</p>Im Bungert 5
+        <p class="font-bold pb-1">Home-Office</p>Im Bungert 5
         <br />54317 Herl
         <br />
       </section>
       <section class="py-4 mr-12">
-        <p class="underline pb-2">Kontakt</p>Mobil: 0151 12 669 832
+        <p class="font-bold pb-1">Kontakt</p>Mobil: 0151 12 669 832
         <br />e-Mail: pe@finanzberatung-ebner.de
       </section>
-      <section class="py-4">
-        <p class="underline pb-2">Links</p>
-        <nuxt-link to="/impressum">Impressum</nuxt-link>
-        <br />
-        <nuxt-link to="/datenschutz">Datenschutz</nuxt-link>
-      </section>
     </div>
+    <section class="py-4">
+      <p class="font-bold pb-1">Links</p>
+      <nuxt-link to="/impressum">Impressum</nuxt-link>
+      <br />
+      <nuxt-link to="/datenschutz">Datenschutz</nuxt-link>
+    </section>
   </div>
 </template>
 
 <script>
-import Logo from "@/components/Logo.vue";
+import LogoWhite from "@/components/LogoWhite.vue";
 export default {
   components: {
-    Logo
+    LogoWhite
   }
 };
 </script>
