3ac0a742ec62f2a5f6a54924ca7981964d21992c
dvdptr <info@davidebner.de>
2020-08-14 16:28:07 +0200

    add google analyitcs module

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ export default {
         hid: "description",
         name: "description",
         content: process.env.npm_package_description || ""
-      }
+      },
+      { hid: "keyword", name: "keyword", content: "Finanzberatung" }
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
--- pages/index.vue
+++ pages/index.vue
@@ -31,10 +31,16 @@ export default {
   },
   head() {
     return {
-      htmlAttrs: {
-        lang: "de",
-      },
-      title: "test",
+      title: "Peter Ebner Finanzberatung Plus: Startseite",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          hid: "description",
+          name: "description",
+          content: process.env.npm_package_description || "",
+        },
+      ],
     };
   },
 };
