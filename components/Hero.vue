7c2bd1435b95ca2b2865ac84247699d084251524
dvdptr <info@davidebner.de>
2021-01-18 14:32:15 +0100

    fix: remove buttons from header, waiting for more content

--- components/Hero.vue
+++ components/Hero.vue
@@ -10,12 +10,12 @@
           class="text-gray-800 text-lg pb-4 leading-snug"
         >Sie benötigen individuelle Beratung für Ihre geplante Finanzierung? Als unabhängiger Finanzierungsspezialist bin ich Ihr Partner rund um die Themen Finanzierung, Finanzieren ohne Eigenkapital und Privatkredite.</p>
         <p class="text-gray-800 text-lg pb-6">Ich bin gerne für Sie da.</p>
-        <button
+        <!-- <button
           class="bg-red-700 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
-        >Anfrage stellen</button>
+        ><a href="mailto:someone@yoursite.com" target="_blank" rel="noopener noreferrer">Anfrage stellen</a></button>
         <button
           class="bg-white hover:bg-red-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow ml-3"
-        >Videoberatung</button>
+        ><nuxt-link  to="/videoberatung">Videoberatung</nuxt-link></button> -->
       </section>
     </div>
     <div class="w-1/2 px-4">
