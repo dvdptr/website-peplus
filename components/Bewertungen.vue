5f644969a5f006133441451d61dee255ef529ed8
dvdptr <info@davidebner.de>
2023-07-05 08:07:36 +0200

    add: bewertung section typo

--- components/Bewertungen.vue
+++ components/Bewertungen.vue
@@ -2,41 +2,34 @@
   <div
     class="pylg border-b mx-auto w-full border-gray-400 py-6 md:w-10/12 md:max-w-screen-xl md:py-10"
   >
+    <Headline :title="title" :subtitle="subtitle" />
     <div class="mx-auto flex-wrap items-center justify-between md:flex">
       <div class="mx-auto items-center sm:w-3/5">
         <section class="mx-auto w-10/12 text-center text-gray-800">
-          <h1>Kundenbewertungen</h1>
-
-          <p class="pt-8 text-base text-gray-800 sm:pt-4">
-            Die Zufriedenheit unserer Kunden steht für uns an oberster Stelle.
-            Lesen Sie hier, was unsere Kunden zu sagen haben.
-          </p>
           <section
-            class="mt-8 rounded-lg bg-red-700 p-4 py-4 text-left text-base font-semibold text-white sm:p-10 sm:py-8"
+            class="mt-8 rounded-lg bg-red-700 p-4 text-left text-base font-semibold text-white sm:p-10"
           >
-            <p>
-              <span class="text-5xl">»</span><br />
-              Zunächst einmal herzlichen Dank an Herrn Ebner und seinen
-              Mitarbeiter Herr Schuster! Wir kannten Herrn Ebner bereits aus
-              früheren Zeiten als angestellten Berater. Als wir erfuhren, dass
-              er nun mit Finanzberatung Plus selbstständig ist, war für uns
-              klar, dass wir wieder zu ihm möchten! Und auch dieses Mal fühlten
-              und fühlen wir uns immer noch in den besten Händen. Herr Ebner
-              nimmt sich Zeit für die Beratung und steht bei allen Anliegen und
-              Fragen mit Rat und Tat zur Seite. Man wird allumfassend beraten
-              und unterstützt. Die Kommunikation mit Banken, die Beantragung
-              etc. übernimmt Herr Ebner und wenn es notwendig ist, auch mit dem
-              nötigen Nachdruck. :-) <br />Selbst nach Auszahlung des Kredites
-              steht Herr Ebner jederzeit zur Verfügung, kümmert sich um weitere
-              Auszahlungen etc. Hier steht der Kunde absolut im Mittelpunkt! Wir
-              hoffen, dass Herr Ebner uns noch lange als Berater zur Seite steht
-              und können die Finanzberatung Plus nur jedem ans Herz legen, der
-              sich bei Finanz- und Kreditfragen gut aufgehoben und ernstgenommen
-              fühlen möchte! Vielen lieben Dank.<br /><span class="text-5xl"
-                >«</span
-              >
-            </p>
+            <span class="text-5xl">»</span><br />
+            Zunächst einmal herzlichen Dank an Herrn Ebner und seinen
+            Mitarbeiter Herr Schuster! Wir kannten Herrn Ebner bereits aus
+            früheren Zeiten als angestellten Berater. Als wir erfuhren, dass er
+            nun mit Finanzberatung Plus selbstständig ist, war für uns klar,
+            dass wir wieder zu ihm möchten! Und auch dieses Mal fühlten und
+            fühlen wir uns immer noch in den besten Händen. Herr Ebner nimmt
+            sich Zeit für die Beratung und steht bei allen Anliegen und Fragen
+            mit Rat und Tat zur Seite. Man wird allumfassend beraten und
+            unterstützt. Die Kommunikation mit Banken, die Beantragung etc.
+            übernimmt Herr Ebner und wenn es notwendig ist, auch mit dem nötigen
+            Nachdruck. :-) <br />Selbst nach Auszahlung des Kredites steht Herr
+            Ebner jederzeit zur Verfügung, kümmert sich um weitere Auszahlungen
+            etc. Hier steht der Kunde absolut im Mittelpunkt! Wir hoffen, dass
+            Herr Ebner uns noch lange als Berater zur Seite steht und können die
+            Finanzberatung Plus nur jedem ans Herz legen, der sich bei Finanz-
+            und Kreditfragen gut aufgehoben und ernstgenommen fühlen möchte!
+            Vielen lieben Dank.<br /><span class="text-5xl">«</span>
+
             <br />
+            <p class="text-2xl text-yellow-400">★★★★★</p>
             <p>Andre & Jasmin, 15.06.2023</p>
             <p>Eingereicht über ProvenExpert</p>
           </section>
@@ -47,7 +40,20 @@
 </template>
 
 <script>
-export default {};
+import Headline from "@/components/elements/Headline.vue";
+
+export default {
+  components: {
+    Headline,
+  },
+  data() {
+    return {
+      title: "Kundenbewertungen",
+      subtitle:
+        " Die Zufriedenheit unserer Kunden steht für uns an oberster Stelle. Wir nehmen uns die Zeit, ihre individuellen Bedürfnisse und Ziele zu verstehen, um ihnen die bestmögliche Beratung zu bieten. Überzeugen Sie sich selbst.",
+    };
+  },
+};
 </script>
 
 <style></style>
